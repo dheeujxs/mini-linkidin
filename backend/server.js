6ef047dd635025d6098707711df1a4a43237cd6a
Dheeraj Goswami <132127084+dheeujxs@users.noreply.github.com>
2025-08-08 20:03:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,7 @@ const allowedOrigins = [
   "https://mini-linkidin-h3th.vercel.app" // deployed frontend
 ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
+
 
 // Handle preflight for all routes
 app.options('*', cors({
